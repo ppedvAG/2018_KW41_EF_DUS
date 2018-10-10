4e8145e8caac42a8a6858d1943561a7caa01bfab
André <andrerrr@outlook.de>
2018-10-10 15:16:03 +0200

    Context V0.01 und Fleischtheke

---
+++ Hallo_EFCodeFirst/Hallo_EFCodeFirst/EfContext.cs
@@ -0,0 +1,16 @@
+﻿using Hallo_EFCodeFirst.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hallo_EFCodeFirst
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Tier> Tiere { get; set; }
+        public DbSet<Mahlzeit> Mahlzeiten { get; set; }
+    }
+}
