c6516042d83a054e46ead85f0bea912db0e380fd
André <andrerrr@outlook.de>
2018-10-11 10:54:39 +0200

    Ef Code Frist

---
+++ ppedv.ElenasUwe/ppedv.ElenasUwe.Data.EF/EfContext.cs
@@ -0,0 +1,24 @@
+﻿using ppedv.ElenasUwe.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.ElenasUwe.Data.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Stoff> Stoffe { get; set; }
+        public DbSet<Zubereitung> Zubereitungen { get; set; }
+        public DbSet<Vorgang> Vorgaenge { get; set; }
+        public DbSet<Produkt> Produkte { get; set; }
+
+        public EfContext() : this("Server=.;Database=EleneasUwe;Trusted_Connections=true")
+        { }
+
+        public EfContext(string nameConString) : base(nameConString)
+        { }
+    }
+}
---
+++ ppedv.ElenasUwe/ppedv.ElenasUwe.Data.EF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ppedv.ElenasUwe.Data.EF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.ElenasUwe.Data.EF")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac6ad403-ccf7-411a-9247-a7a8ed41697a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
